d057ee9ea1b2128318c59b80b9de538cd5622d9f
SergioRudin1609 <sergiorudinTEST@gmail.com>
2026-06-11 20:59:47 -0600

    commit semana 5

--- TiendaTechh/src/main/java/com/tiendaTech/tienda/repository/CategoriaRepository.java
+++ TiendaTechh/src/main/java/com/tiendaTech/tienda/repository/CategoriaRepository.java
@@ -13,5 +13,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
     public List<Categoria> findByActivoTrue();
+    
+ 
 }
  
