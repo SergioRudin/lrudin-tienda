95499a8498920ff37ea639b702e141a65f2dc9a5
SergioRudin1609 <sergiorudinTEST@gmail.com>
2026-07-14 08:54:12 -0600

    semana 8/9

--- TiendaTechh/src/main/java/com/tiendaTech/tienda/controller/ConsultaController.java
+++ TiendaTechh/src/main/java/com/tiendaTech/tienda/controller/ConsultaController.java
@@ -1,49 +1,60 @@
 package com.tiendaTech.tienda.controller;
 
-import com.tiendaTech.tienda.service.CategoriaService;
 import com.tiendaTech.tienda.service.ProductoService;
+import java.math.BigDecimal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/consultas")
 public class ConsultaController {
 
     private final ProductoService productoService;
-    private final CategoriaService categoriaService;
 
-    public ConsultaController(ProductoService productoService, CategoriaService categoriaService) {
+    public ConsultaController(ProductoService productoService) {
         this.productoService = productoService;
-        this.categoriaService = categoriaService;
     }
 
     @GetMapping("/listado")
     public String listado(Model model) {
         var productos = productoService.getProductos(false);
-        var categorias = categoriaService.getCategorias(false);
-
         model.addAttribute("productos", productos);
-        model.addAttribute("categorias", categorias);
-
         return "/consultas/listado";
     }
 
-    @PostMapping("/consultaCategoria")
-    public String consultaCategoria(@RequestParam Integer idCategoria,
-            @RequestParam double precioInf,
-            @RequestParam double precioSup,
-            Model model) {
-
-        var lista = productoService.consultaCategoria(idCategoria, precioInf, precioSup);
-        var categorias = categoriaService.getCategorias(false);
-
+    @PostMapping("/consultaDerivada")
+    public String consultaDerivada(@RequestParam() double precioInf,
+            @RequestParam() double precioSup, Model model) {
+        var lista = productoService.consultaDerivada(precioInf, precioSup);
         model.addAttribute("productos", lista);
-        model.addAttribute("categorias", categorias);
         model.addAttribute("precioInf", precioInf);
         model.addAttribute("precioSup", precioSup);
-        model.addAttribute("idCategoria", idCategoria);
-
         return "/consultas/listado";
     }
+
+    @PostMapping("/consultaJPQL")
+    public String consultaJPQL(@RequestParam() double precioInf,
+            @RequestParam() double precioSup,
+            Model model) {
+        var productos = productoService.consultaJPQL(precioInf, precioSup);
+        model.addAttribute("productos", productos);
+        model.addAttribute("precioInf", precioInf);
+        model.addAttribute("precioSup", precioSup);
+        return "/consultas/listado";
+    }
+
+    @PostMapping("/consultaSQL")
+    public String consultaSQL(@RequestParam() double precioInf,
+            @RequestParam() double precioSup, Model model) {
+        var lista = productoService.consultaSQL(precioInf, precioSup);
+        model.addAttribute("productos", lista);
+        model.addAttribute("precioInf", precioInf);
+        model.addAttribute("precioSup", precioSup);
+        return "/consultas/listado";
+    }
+
 }
--- TiendaTechh/src/main/java/com/tiendaTech/tienda/repository/ProductoRepository.java
+++ TiendaTechh/src/main/java/com/tiendaTech/tienda/repository/ProductoRepository.java
@@ -1,24 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.tiendaTech.tienda.repository;
 
 import com.tiendaTech.tienda.domain.Producto;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;    
-
-
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductoRepository extends JpaRepository<Producto, Integer>{
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+
     public List<Producto> findByActivoTrue();
-    
-        //Ejemplo de método utilizando consultas derivadas
+
+    //Ejemplo de método utilizando consultas derivadas
     public List<Producto> findByPrecioBetweenOrderByPrecioAsc(double precioInf, double precioSup);
 
     //Ejemplo de método utilizando consultas JPQL
@@ -29,13 +23,5 @@ public interface ProductoRepository extends JpaRepository<Producto, Integer>{
     @Query(nativeQuery = true,
             value = "SELECT * FROM producto p WHERE p.precio BETWEEN :precioInf AND :precioSup ORDER BY p.precio ASC")
     public List<Producto> consultaSQL(@Param("precioInf") double precioInf, @Param("precioSup") double precioSup);
- @Query(nativeQuery = true,
-        value = "SELECT * FROM producto p "
-        + "WHERE p.id_categoria = :idCategoria "
-        + "AND p.precio BETWEEN :precioInf AND :precioSup "
-        + "ORDER BY p.precio ASC")
-public List<Producto> consultaCategoria(
-        @Param("idCategoria") Integer idCategoria,
-        @Param("precioInf") double precioInf,
-        @Param("precioSup") double precioSup);
+
 }
